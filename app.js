0c14a832bfa8330d5779e9587b4fb2fec03f6acb
Emmanuel Yolou <emmanuelyolou@gmail.com>
2022-08-02 19:55:58 +0000

    close cookie animation done

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const tl = gsap.timeline({defaults: { Duration: 0.75}});
-tl.fromTo(".cookie-container", { scale: 0 }, { scale: 1});
-tl.fromTo(".cookie", { opacity: 0, x:-40, rotate: -45 }, { opacity: 1, rotate: 0, x: 0 });
-tl.fromTo(".cookie-text", { x: 50, opacity: 0 }, { x: 0, opacity: 1});
+const tl = gsap.timeline({defaults: { Duration: 0.75, ease: "power1.out"}});
+tl.fromTo(".cookie-container", { scale: 0 }, { scale: 1, ease: "elastic.out(1, 0.4)", duration: 1.5});
+tl.fromTo(".cookie", { opacity: 0, x:-40, rotate: -45 }, { opacity: 1, rotate: 0, x: 0 }, '<55%');
+tl.fromTo(".cookie-text", { x: 50, opacity: 0 }, { x: 0, opacity: 1}, '<');
+
+const button = document.querySelector('button');
+const cookieContainer = document.querySelector('cookie-container');
+button.addEventListener('click', () => { 
+    gsap.to('.cookie-container', {y: 100, opacity: 0});
+})
