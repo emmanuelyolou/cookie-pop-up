e32d263d54b8b43a5328c145eb7704f28db3a0f4
Emmanuel Yolou <emmanuelyolou@gmail.com>
2022-08-02 20:27:24 +0000

    infinite cookie and crumbs animation implemented

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const tl = gsap.timeline({defaults: { Duration: 0.75, ease: "power1.out"}});
 tl.fromTo(".cookie-container", { scale: 0 }, { scale: 1, ease: "elastic.out(1, 0.4)", duration: 1.5});
-tl.fromTo(".cookie", { opacity: 0, x:-40, rotate: -45 }, { opacity: 1, rotate: 0, x: 0 }, '<55%');
+tl.fromTo(".cookie", { opacity: 0, x:-40, rotation: "-45deg" }, { opacity: 1, rotation: "0deg", x: 0 }, '<50%');
 tl.fromTo(".cookie-text", { x: 50, opacity: 0 }, { x: 0, opacity: 1}, '<');
+tl.fromTo(".cookie", { y: 0, rotation: "0deg" }, { y: -20, rotation: "-10deg", yoyo: true, repeat: -1 }, );
+tl.fromTo("#crumbs", { y: 0 }, { y: -12, rotation: "-5deg", yoyo: true, repeat: -1 }, '<');
 
 const button = document.querySelector('button');
 const cookieContainer = document.querySelector('cookie-container');
 button.addEventListener('click', () => { 
-    gsap.to('.cookie-container', {y: 100, opacity: 0});
+    gsap.to('.cookie-container', {y: 30, opacity: 0});
 })
